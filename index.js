8d8f10392a2dfe3daa4d1ec0fcbe0baf53df73e0
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2025-02-02 18:42:02 +0200

    Delete Comments
    Removed unnecessary comments

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-        /**
-         * Challenge: get a URL for a default background image and set it here
-         * 
-         * 1. Change the query in the URL above back to something real
-         * 2. Log the image url to the console (replacing console.log(data) above)
-         * 3. Use that URL as the "default" background image to be used if 
-         *    the promise is ever rejected.
-         */
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1533221498444-b1aa65fcc827?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Mzg1MTEwNTN8&ixlib=rb-4.0.3&q=80&w=1080)`
     })
 
