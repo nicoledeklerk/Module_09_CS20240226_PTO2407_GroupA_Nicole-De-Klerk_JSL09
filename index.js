13e1892057ba2c1e19fd7ff26df6a499c81e8f68
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2025-02-02 17:49:26 +0200

    Added basic layout and dynamic background image functionality
    Added Unsplash API integration for random nature backgrounds with author attribution and fallback image handling

--- index.js
+++ index.js
@@ -1,63 +1,20 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
+        console.log(data.urls.regular)
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
+        /**
+         * Challenge: get a URL for a default background image and set it here
+         * 
+         * 1. Change the query in the URL above back to something real
+         * 2. Log the image url to the console (replacing console.log(data) above)
+         * 3. Use that URL as the "default" background image to be used if 
+         *    the promise is ever rejected.
+         */
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1533221498444-b1aa65fcc827?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Mzg1MTEwNTN8&ixlib=rb-4.0.3&q=80&w=1080)`
     })
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
-        `
-    })
-    .catch(err => console.error(err))
-
-// function getCurrentTime() {
-//     const date = new Date()
-//     document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
-// }
-
-// setInterval(getCurrentTime, 1000)
-
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            document.getElementById("weather").innerHTML = `
-                <img src=${iconUrl} />
-                <p>${Math.round(data.main.temp)}º</p>
-                <p>${data.name}</p>
-            `
-        })
-        .catch(err => console.error(err))
-});
-
-/**
- * Challenge: Try to lay out the weather similar to how
- * Momentum does it.
- */
+    
