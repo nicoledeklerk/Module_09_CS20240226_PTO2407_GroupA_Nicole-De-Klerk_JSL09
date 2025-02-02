c833b7c23df9d0fede998b55ce5a555d3622749c
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2025-02-02 17:59:52 +0200

    Update JavaScript to fetch cryptocurrency data
    Added a new fetch request to CoinGecko API to retrieve bitcoin data - included error handling

--- index.js
+++ index.js
@@ -17,4 +17,15 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1533221498444-b1aa65fcc827?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Mzg1MTEwNTN8&ixlib=rb-4.0.3&q=80&w=1080)`
     })
 
-    
+    fetch ("https://api.coingecko.com/api/v3/coins/bitcoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        console.log(res.status)
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => console.error(err))
